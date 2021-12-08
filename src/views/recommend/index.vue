ae7af52926cdb0723b718ee2c2b71f311cd809a8
Linosau <liuf@liuf.com>
2021-12-08 13:47:36 +0800

    feat: 完成推荐首页面展示

--- src/views/recommend/index.vue
+++ src/views/recommend/index.vue
@@ -7,33 +7,69 @@
  * @FilePath: /vue3-music/src/views/recommend/index.vue
 -->
 <template>
-  <div class="recommend">
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <slider v-if="sliders.length" :sliders="sliders" />
+  <div class="recommend" v-loading="loading">
+    <scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <slider v-if="sliders.length" :sliders="sliders" />
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 v-show="!loading" class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li
+              class="item"
+              v-for="item in albums"
+              :key="item.id"
+              @click="selectItem(item)"
+            >
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.pic">
+              </div>
+              <div class="text">
+                <h2 class="name">{{ item.username }}</h2>
+                <p class="title">{{item.title}}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import Slider from '@/components/base/Slider'
+import Scroll from '@/components/base/Scroll'
 import { getRecommend } from '@/api/recommend'
 
 export default {
   name: 'recommend',
   components: {
-    Slider
+    Slider,
+    Scroll
+  },
+  async created () {
+    const res = await getRecommend()
+    console.log(res)
+    this.sliders = res.sliders
+    this.albums = res.albums
   },
   data: () => ({
     sliders: [],
     albums: [],
     selectedAlbum: null
   }),
-  async created () {
-    const res = await getRecommend()
-    this.sliders = res.sliders
-    this.albums = res.albums
+  computed: {
+    loading () {
+      return !this.sliders.length && !this.albums.length
+    }
+  },
+  methods: {
+    selectItem (album) {
+      console.log(album)
+    }
   }
 }
 </script>
@@ -58,6 +94,46 @@ export default {
       top: 0;
       width: 100%;
       height: 100%;
+    }
+    &-content {
+      overflow: hidden;
+      height: 100%;
+    }
+    &-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+        }
+        .name {
+          margin-bottom: 10px;
+          color: $color-text;
+        }
+        .title {
+          color: $color-text-d;
+        }
+      }
     }
   }
 </style>
