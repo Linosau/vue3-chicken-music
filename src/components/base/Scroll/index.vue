f8560bcb7983ead44926e7be20512358c7e1daed
Linosau <liuf@liuf.com>
2021-12-08 13:46:13 +0800

    feat: 完善Scroll组件

--- src/components/base/Scroll/index.vue
+++ src/components/base/Scroll/index.vue
@@ -14,12 +14,24 @@
 
 <script type="text/ecmascript-6">
 import { ref } from 'vue'
-// import useScroll from './use-scroll'
+import useScroll from './use-scroll'
 
 export default {
   name: 'Scroll',
-  setup () {
+  props: {
+    click: {
+      type: Boolean,
+      default: true
+    },
+    probeType: {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: ['scroll'],
+  setup (props, { emit }) {
     const rootRef = ref(null)
+    useScroll(rootRef, props, emit)
 
     return {
       rootRef
--- src/components/base/Scroll/use-scroll.js
+++ src/components/base/Scroll/use-scroll.js
@@ -7,16 +7,37 @@
  * @FilePath: /vue3-music/src/components/base/Scroll/use-scroll.js
  */
 import BScroll from '@better-scroll/core'
-import { onMounted, onUnmounted, ref } from 'vue'
+import ObserveDOM from '@better-scroll/observe-dom'
+import { onMounted, onUnmounted, onActivated, onDeactivated, ref } from 'vue'
 
-export default function useScroll (wrapperRef) {
+BScroll.use(ObserveDOM)
+
+export default function useScroll (wrapperRef, options, emit) {
   const scroll = ref(null)
 
   onMounted(() => {
-    scroll.value = new BScroll(wrapperRef.value)
+    const scrollVal = scroll.value = new BScroll(wrapperRef.value, {
+      observeDOM: true,
+      ...options
+    })
+
+    if (options.probeType > 0) {
+      scrollVal.on('scroll', (pos) => {
+        emit('scroll', pos)
+      })
+    }
   })
 
   onUnmounted(() => {
     scroll.value.destroy()
+  })
+
+  onActivated(() => {
+    scroll.value.enable()
+    scroll.value.refresh()
+  })
+
+  onDeactivated(() => {
+    scroll.value.disable()
   })
 }
