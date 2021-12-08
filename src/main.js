0e464aef0fb909beeb08a99437212f7879ada17e
Linosau <liuf@liuf.com>
2021-12-08 13:45:22 +0800

    feat: 添加自定义指令Loading

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import lazyPlugin from 'vue3-lazy'
+import LoadingDirective from '@/components/directive/Loading/directive'
 import '@/styles/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store).use(router)
+  .use(lazyPlugin, { loading: require('@/assets/images/default.png') })
+  .directive('loading', LoadingDirective)
+  .mount('#app')
